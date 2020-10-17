5bc6de4fac37ed4435acbea57fcb3f171b9a5e1a
pn8128 <1persongloria@gmail.com>
2020-10-17 14:53:48 +0900

    import を容易にした

--- ChemNote/__init__.py
+++ ChemNote/__init__.py
@@ -1,3 +1,11 @@
 from .ChemNum import ChemNumBuilder
 
-ChemNum4Jupyter = ChemNumBuilder(ChemNumBuilder.printMarkdown)
+ChemNum4Jupyter = ChemNumBuilder()
+
+
+def define(*arg):
+    return ChemNum4Jupyter.define(*arg)
+
+
+def exp(*arg):
+    return ChemNum4Jupyter.exp(*arg)
